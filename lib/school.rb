176d48e3d270629970597125628001e5e4aa9cdc
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-05-01 01:35:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -21,15 +21,6 @@ class School
     end 
   end 
   
-# This works, but it isn't passing learn.co's tests cause of 
-# how it displays syntactically...but the same information is there
-#  def sort 
-#    roster.sort_by do |x, y| 
-#      x 
-#    end 
-#  end 
-
-# Here's learn.co's solution:
 def sort 
   nu_hash = {}
   roster.each do |x, y| 
